446bba34c77c33a75507325c8184b6da248db762
Huseyn Mikayil <huseyn-mikayil@mail.ru>
2020-06-22 23:09:29 +0400

    redux store and provider implemented

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import SearchBar from './components/layout/SearchBar';
 import Notes from './components/notes/Notes';
 import AddNote from './components/notes/AddNote';
 import EditNoteModal from './components/notes/EditNoteModal';
+import { Provider } from 'react-redux';
+import store from './store';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js'
@@ -15,14 +17,16 @@ function App() {
   });
 
   return (
-    <Fragment>
-      <SearchBar />
-      <div className="container">
-        <AddNote />
-        <EditNoteModal />
-        <Notes />
-      </div>
-    </Fragment>
+    <Provider store={store}>
+      <Fragment>
+        <SearchBar />
+        <div className="container">
+          <AddNote />
+          <EditNoteModal />
+          <Notes />
+        </div>
+      </Fragment>
+    </Provider>
   );
 }
 
